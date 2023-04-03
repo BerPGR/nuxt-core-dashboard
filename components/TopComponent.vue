2eb1ea0498e0f01f99c76a3eba90d9de13f3441c
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-04-03 16:11:52 -0300

    Added fonts and created top component

--- components/TopComponent.vue
+++ components/TopComponent.vue
@@ -1,13 +1,17 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <TopComponent />
+        
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+
 }
 </script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -17,11 +17,18 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Ubuntu&display=swap",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/fonts.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
